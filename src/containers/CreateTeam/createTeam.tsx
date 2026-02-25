6fd5d350e8361b6895d01f1fa93f12dd2163db2b
Vinícius Gabriel <viniciusgabriel102009@gmail.com>
2026-02-25 10:02:53 -0300

    feat(team): add create team

--- src/containers/CreateTeam/createTeam.tsx
+++ src/containers/CreateTeam/createTeam.tsx
@@ -1,7 +1,51 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function CreateTeam() {
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const data = {
+      name
+    };
+
+    try {
+      const response = await fetch("http://localhost:8080/teams", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error("Erro ao criar time");
+      }
+
+      setName("");
+      
+      navigate("/dashboard");
+    } catch (error) {
+      console.error(error);
+      alert("Erro ao criar time");
+    }
+  }
+
   return (
-    <>
-      <h1>Create Team</h1>
-    </>
+    <form onSubmit={handleSubmit}>
+      <h1>Criar time</h1>
+      <label htmlFor="name">Nome do time</label>
+      <input
+        type="text"
+        id="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button>Criar</button>
+    </form>
   );
 }
